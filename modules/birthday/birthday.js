93f73f27d2e2ebb743dc8e3386c8a7617326bf60
amki <sirhelmchen@gmail.com>
2018-03-24 02:20:02 +0100

    Add configs/template for main

--- modules/birthday/birthday.js
+++ modules/birthday/birthday.js
@@ -5,10 +5,8 @@ var birthday = function(abot) {
     self.checkDbInit = function() {
         sqlite.db.run("CREATE TABLE IF NOT EXISTS birthday(tag TEXT PRIMARY KEY, date INTEGER NOT NULL)");
     };
-    self.saveBirthday = async function(tag, date) {
-    }
     self.checkDbInit();
-    command.addCommand("addbday","",async function(msg, args) {
+    command.addCommand("bdayadd","",async function(msg, args) {
         if(args.length != 2) {
             msg.channel.send("Usage: !addbday [tag] [date].");
             return;
@@ -18,7 +16,6 @@ var birthday = function(abot) {
             return;
         }
         var buser = msg.mentions.users.first();
-        console.log("Tag was: ",buser.tag);
         var timestamp=Date.parse(args[1]);
         if(isNaN(timestamp)) {
             msg.channel.send("Sorry I couldn't parse that date.");
@@ -29,8 +26,37 @@ var birthday = function(abot) {
         bdate.setMinutes(0);
         bdate.setSeconds(0);
         bdate.setMilliseconds(0);
-        await self.saveBirthday(bdate);
-        msg.channel.send("addbday: "+bdate);
+        sqlite.db.run("INSERT OR REPLACE INTO birthday (tag, date) VALUES ($tag, $date)",{"$tag": buser.tag, "$date": bdate.getTime()}, function(err) {
+            if(err) {
+                console.log("Birthday ERROR inserting into birthday table: ",err);
+                msg.channel.send("I could not save this birthday: "+err.message);
+                return;
+            }
+            msg.channel.send("I saved "+bdate+" as birthday for "+buser);
+        });
+    });
+
+    command.addCommand("bdayshow","",async function(msg, args) {
+        if(args.length != 1) {
+            msg.channel.send("Usage: !addbday [tag].");
+            return;
+        }
+        if(msg.mentions.users.array().length > 1) {
+            msg.channel.send("Please only mention one user at a time.");
+            return;
+        }
+        var buser = msg.mentions.users.first();
+        sqlite.db.all("SELECT * FROM birthday WHERE tag = $tag",{"$tag": buser.tag}, function(err,rows) {
+            if(rows.length < 1) {
+                msg.channel.send("I don't know when "+buser+"'s birthday is. Add it with !bdayadd. :)");
+                return;
+            }
+            // tag is UNIQUE, can only be 1 hit
+            var row = rows[0];
+            var bdate = new Date(row.date);
+            msg.channel.send("I have "+bdate+" saved as birthday for "+buser);
+        });
+
     });
 };
 
